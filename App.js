0f7e53e52402b399fc715932e495874e901fd52f
tamagosglaiza6-sketch <tamagosglaiza6@gmail.com>
2026-04-23 22:39:28 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,7 @@
+function postAnnouncement() {
+   
+    let message = document.getElementById("inputBox").value;
+
+    document.getElementById("outputBox").innerText = message;
+}
+Write to Stephany Granada Boloy
